6899f228fd692b2e3d5946b7ce3dc3bb59a0f5d9
Morris <106055094+MorrisrCodes@users.noreply.github.com>
2025-02-27 11:54:06 -0500

    completed in class php assignment

---
+++ class practice/php/kitties.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>kitties</title>
+    </head>
+    <body>
+        <div>
+            <?php
+                for ($i =1; $i < 6; $i++) {
+                    print("<img src='images/kitty$i.jpeg'>");
+                }
+            ?>
+        </div>
+    </body>
+</html>
